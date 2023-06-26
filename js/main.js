dbf7b19bcfdcb0abd1873ffc65a2612b350a37fd
Retekh <retekh@gmail.com>
2023-06-26 14:46:06 -0500

    removed most of js code to start over

--- js/main.js
+++ js/main.js
@@ -1,38 +1,4 @@
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
-}
+let canvas = document.getElementById('screen');
+let context = canvas.getContext('2d');
 
-class SpriteSheet {
-    constructor(image ,width, height) {
-        this.image = image;
-        this.width = width;
-        this.height = height;
-    }
-    
-}
-
-
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
-
-
-
-loadImage('/img/mario_tile_set.png')
-.then(image => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define('ground', 0, 0);
-    sprites.draw('ground', context, 45, 62);
-    
-    context.drawImage(0, 0, 16, 16,
-                      32, 32, 16, 16);
-
-})
-.catch(error => {
-    console.error('Error with image.');
-});
+context.fillRect(100, 100, 100, 100);
